7b23e5cdb474bb9505c4703afbe37e16d709e26d
janavitvar <62616130+janavitvar@users.noreply.github.com>
2022-11-15 13:02:27 +0100

    Add Swap

---
+++ src/christmas/Swap.java
@@ -0,0 +1,16 @@
+package week01_introduction;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a = 34;
+        int b;
+        b = 89;
+        
+        int c;
+        c = a;
+        a = b;
+        b = c;
+        System.out.println(a + " " + b);
+        //System.out.println(b + " " + a);
+    }
+}
